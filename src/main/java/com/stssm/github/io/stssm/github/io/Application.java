8b6fc35f1f434277a90dc5bc5e82768908371c96
liujiajun <45444154+wo1261931780@users.noreply.github.com>
2022-07-30 14:00:03 +0800

    init

---
+++ src/main/java/com/stssm/github/io/stssm/github/io/Application.java
@@ -0,0 +1,13 @@
+package com.stssm.github.io.stssm.github.io;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/stssm/github/io/stssm/github/io/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stssm.github.io.stssm.github.io;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
