a06c28de01b14395d153a3780317dd4fbba5a655
liujiajun <45444154+wo1261931780@users.noreply.github.com>
2022-08-15 22:26:09 +0800

    bean生命周期

--- src/main/java/com/stssm/github/io/junw/bbb001spring20220730/Bbb007.java
+++ src/main/java/com/stssm/github/io/junw/bbb001spring20220730/Bbb007.java
@@ -19,5 +19,24 @@ public class Bbb007 {
 		xmlDemoService7.servieShow7();
 		System.out.println("下面开始执行关闭=============================================");
 		context.close();
+		// 完整的过程如下：
+		// 我是dao7构造方法，地址为：com.stssm.github.io.junw.bbb001spring20220730.dao.Impl.DemoDao7Impl@49ec71f8
+		// 我是service7中的DI注入
+		// 我是service7中的初始化方法
+		// 我是service中的show7方法
+		// 我是service中的Dao7对象：com.stssm.github.io.junw.bbb001spring20220730.dao.Impl.DemoDao7Impl@49ec71f8
+		// 开始调用Dao中的show方法：--------------------------------------
+		// 我是dao方法中的show7
+		// 下面开始执行关闭=============================================
+		// 21:35:31.989 [main] DEBUG org.springframework.context.support.ClassPathXmlApplicationContext - Closing org.springframework.context.support.ClassPathXmlApplicationContext@4411d970, started on Mon Aug 15 21:35:31 CST 2022
+		// ************************************************************************************************
+		// ************************************************************************************************
+		// ************************************************************************************************
+		// 整体来看一遍
+		// 首先是xml中配置的两个bean对象，被放入IoC统一管理。
+		// 1.构造方法被加载出来
+		// 2.service中配置的依赖注入出现
+
+
 	}
 }
