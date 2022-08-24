a431996d41b17b0791d4e86409a2c02764a8dee2
liujiajun <45444154+wo1261931780@users.noreply.github.com>
2022-08-24 22:09:57 +0800

    day2-1

--- src/main/java/com/stssm/github/io/junw/bbb004spring20220802/Bbb015.java
+++ src/main/java/com/stssm/github/io/junw/bbb004spring20220802/Bbb015.java
@@ -16,8 +16,11 @@ import javax.sql.DataSource;
  */
 public class Bbb015 {
 	public static void main(String[] args) {
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("bbb102SpringContext.xml");
-		DataSource dataSource = (DataSource) ctx.getBean("dataSource");
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("bbb016DataSource.xml");
+		DataSource dataSource = (DataSource) ctx.getBean("xmlDataSource");
+		// 这里有点不一样
+		// 我在xml中配置的对象名，重构之后不会在这里出现
+		// 不会换句话说，这里是不是将双引号的内容全部作为字符串处理？
 		System.out.println(dataSource);
 		System.out.println("******************************************");
 		DataSource dataSource2 = (DataSource) ctx.getBean("dataSource2");
