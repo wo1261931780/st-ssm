fd6fce49732bc41da0fa9f593bf3264d2c9afdf1
liujiajun <45444154+wo1261931780@users.noreply.github.com>
2022-09-26 00:11:58 +0800

    day2-13

---
+++ src/main/java/com/stssm/github/io/junw/bbb009spring20220919/dao/Demo0919Dao001.java
@@ -0,0 +1,13 @@
+package com.stssm.github.io.junw.bbb009spring20220919.dao;
+
+/**
+ * Created by Intellij IDEA.
+ * Project:st-ssm.github.io
+ * Package:com.stssm.github.io.junw.bbb009spring20220919.dao
+ *
+ * @author liujiajun_junw
+ * @Date 2022-09-22-40  星期日
+ * @description
+ */
+public class Demo0919Dao001 {
+}
