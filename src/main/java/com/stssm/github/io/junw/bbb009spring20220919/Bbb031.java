fc920adc8982f8897a511882b7b26426892b5357
liujiajun <45444154+wo1261931780@users.noreply.github.com>
2022-09-30 16:51:01 +0800

    解决sonarlint问题

--- src/main/java/com/stssm/github/io/junw/bbb009spring20220919/Bbb031.java
+++ src/main/java/com/stssm/github/io/junw/bbb009spring20220919/Bbb031.java
@@ -1,7 +1,7 @@
 package com.stssm.github.io.junw.bbb009spring20220919;
 
 import com.stssm.github.io.junw.bbb009spring20220919.config.Spring0919Configuration001;
-import com.stssm.github.io.junw.bbb009spring20220919.dao.Impl.Demo0919Dao001Impl;
+import com.stssm.github.io.junw.bbb009spring20220919.dao.Demo0919Dao001;
 import com.stssm.github.io.junw.bbb009spring20220919.domain.Account0919;
 import com.stssm.github.io.junw.bbb009spring20220919.service.Demo0919Service001;
 import org.springframework.context.ApplicationContext;
@@ -22,7 +22,7 @@ public class Bbb031 {
 	public static void main(String[] args) {
 		// ApplicationContext x = new AnnotationConfigApplicationContext(Spring0919Configuration001.class);
 		ApplicationContext x = new AnnotationConfigApplicationContext(Spring0919Configuration001.class);
-		Demo0919Dao001Impl bean = x.getBean(Demo0919Dao001Impl.class);
+		Demo0919Dao001 bean = x.getBean(Demo0919Dao001.class);
 		bean.daoShow001();
 		Demo0919Service001 bean1 = x.getBean(Demo0919Service001.class);
 		Account0919 account0919Dao = bean1.selectById(2);
--- src/main/java/com/stssm/github/io/junw/bbb009spring20220919/dao/Demo0919Dao001.java
+++ src/main/java/com/stssm/github/io/junw/bbb009spring20220919/dao/Demo0919Dao001.java
@@ -1,5 +1,11 @@
 package com.stssm.github.io.junw.bbb009spring20220919.dao;
 
+import com.stssm.github.io.junw.bbb009spring20220919.domain.Account0919;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
 /**
  * Created by Intellij IDEA.
  * Project:st-ssm.github.io
@@ -11,4 +17,25 @@ package com.stssm.github.io.junw.bbb009spring20220919.dao;
  */
 public interface Demo0919Dao001 {
 	void daoShow001();
+
+
+	@Insert("insert into study001sql.demo1(id, name, age, sex, address, math, english, hire_date) VALUES(#{})")
+	void addDao0919();
+
+	@Delete("delete from study001sql.demo1 where id=#{demoId}")
+	Integer deleteById(Integer demoId);
+
+	@Update("update study001sql.demo1 set name  =#{demoName}  where id =#{demoId} ;")
+	Integer updateByCondition(Integer demoId, String demoName);
+
+
+	/**
+	 * 根据id查询结果
+	 *
+	 * @param demoId
+	 */
+	@Select("select * from study001sql.demo1 where id=#{demoId};")
+	Account0919 daoSelectById(Integer demoId);
+
+
 }
