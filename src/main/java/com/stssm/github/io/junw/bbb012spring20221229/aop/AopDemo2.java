b5d46bd5abffbd7a8e846e10e63f5b20ae3a92bc
junw <45444154+wo1261931780@users.noreply.github.com>
2023-01-02 10:02:40 +0800

    1

--- src/main/java/com/stssm/github/io/junw/bbb012spring20221229/aop/AopDemo2.java
+++ src/main/java/com/stssm/github/io/junw/bbb012spring20221229/aop/AopDemo2.java
@@ -25,19 +25,19 @@ import static com.stssm.github.io.junw.bbb001spring20220730.Bbb002.demorun;
 @Aspect
 public class AopDemo2 {
 
-	@Pointcut("execution(void com.stssm.github.io.junw.bbb011spring20221226.dao.impl.Demo1226DaoImpl.show())")
-	private void cutIn1() {
-	}
-
-	@Around("AopDemo2.cutIn1()")
-	private void around1(ProceedingJoinPoint pjp) throws Throwable {
-		demorun.debug("我是切入点周围的方法");
-		pjp.proceed();
-		demorun.debug("执行完毕");
-		demorun.debug(FINAL_SPLIT);
-		Signature pjpSignature = pjp.getSignature();
-		demorun.debug("我是代理对象：" + pjpSignature.getDeclaringType());
-		demorun.debug("我是对象名称：" + pjpSignature.getDeclaringTypeName());
-		demorun.debug("我是代理方法：" + pjpSignature.getName());
-	}
+	// @Pointcut("execution(void com.stssm.github.io.junw.bbb011spring20221226.dao.impl.Demo1226DaoImpl.show())")
+	// private void cutIn1() {
+	// }
+	//
+	// @Around("AopDemo2.cutIn1()")
+	// private void around1(ProceedingJoinPoint pjp) throws Throwable {
+	// 	demorun.debug("我是切入点周围的方法");
+	// 	pjp.proceed();
+	// 	demorun.debug("执行完毕");
+	// 	demorun.debug(FINAL_SPLIT);
+	// 	Signature pjpSignature = pjp.getSignature();
+	// 	demorun.debug("我是代理对象：" + pjpSignature.getDeclaringType());
+	// 	demorun.debug("我是对象名称：" + pjpSignature.getDeclaringTypeName());
+	// 	demorun.debug("我是代理方法：" + pjpSignature.getName());
+	// }
 }
