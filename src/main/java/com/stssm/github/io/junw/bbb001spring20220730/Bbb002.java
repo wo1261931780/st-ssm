78329bf133f1afcd252ec7703277a33ef9c1f419
junw <45444154+wo1261931780@users.noreply.github.com>
2023-02-11 13:01:33 +0800

    Update Bbb002.java

--- src/main/java/com/stssm/github/io/junw/bbb001spring20220730/Bbb002.java
+++ src/main/java/com/stssm/github/io/junw/bbb001spring20220730/Bbb002.java
@@ -18,7 +18,6 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Bbb002 {
 	public static final Logger demorun = LoggerFactory.getLogger("demorun.class");
 	public static final String FINAL_SPLIT = "=============================================";
-
 	public static void main(String[] args) {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("bbb002.xml");// 去xml中寻找，我已经配置好的文件
 		// 而这个文件，对应的就是一个已经配置好的ioc容器
