a337a9595d111dfb87f23cb2b4e3d1ce3ae3618f
junw <45444154+wo1261931780@users.noreply.github.com>
2023-02-26 21:19:57 +0800

    2

---
+++ src/main/java/wo1261931780/stssm/StSsmApplication.java
@@ -0,0 +1,13 @@
+package wo1261931780.stssm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StSsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StSsmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wo1261931780/stssm/StSsmApplicationTests.java
@@ -0,0 +1,13 @@
+package wo1261931780.stssm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StSsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
