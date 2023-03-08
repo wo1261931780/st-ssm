25567f3e85e267c96711bec77991ca9f59019361
junw <45444154+wo1261931780@users.noreply.github.com>
2023-03-08 09:29:03 +0800

    整理代码

--- src/main/java/wo1261931780/stssm/junw/bbb015spring20230103/springMvc0103Demo/src/main/java/junw555/configuration/springMvcConfiguration.java
+++
@@ -1,9 +0,0 @@
-package junw555.configuration;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("junw555.controller")
-public class springMvcConfiguration {
-}
